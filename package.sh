de2899606bb3f4e5de16304068ee27c395ee2fd3
nobodypb <500155+nobodypb@users.noreply.github.com>
2020-08-30 09:01:32 +0200

    Fixed packaging

--- package.sh
+++ package.sh
@@ -2,13 +2,17 @@
 
 rm -rf node_modules
 npm install --production
-rm -f SHA256SUMS
-sha256sum manifest.json package.json src/*.js src/devices/*.js LICENSE README.md > SHA256SUMS
 rm -rf node_modules/.bin
+
+sha256sum manifest.json package.json src/*.js src/devices/*.js LICENSE README.md > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
 cp -r node_modules ./package
 tar czf ${TARFILE} package
+
+rm SHA256SUMS
 rm -rf package
+
 echo "Created ${TARFILE}"
